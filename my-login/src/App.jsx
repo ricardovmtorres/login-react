3d9a7a6d79998b4f78c86c3bd81e2471e2dc9e0c
Ricardo Torres <ricardo@yardim.com.br>
2022-03-03 18:45:20 -0300

    init

---
+++ my-login/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+// import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPages';
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="app">
+      {/* <h1>App</h1>     */}
+      <LoginPage />
+      {/* <HomePage /> */}
+    </div>
+  );
+}
+
+export default App;
